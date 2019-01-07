9f0a6df4c56f2d58d7ebaa2581c1c958a7806af0
Eddy Rubio <eddyrubioc@gmail.com>
2019-01-06 22:18:25 -0600

    Added TokenID to payment source model.

--- models/paymentSource.go
+++ models/paymentSource.go
@@ -3,6 +3,7 @@ package conekta
 // PaymentSource ...
 type PaymentSource struct {
 	ID       string `json:"id"`
+	TokenID  string `json:"token_id"`
 	Type     string `json:"type"`
 	Deleted  bool   `json:"deleted"`
 	ParentID string `json:"parent_id"`
